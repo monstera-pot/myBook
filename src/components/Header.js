1d0fdbdddd5a858d3f6e1142b0cf03929589d2a2
Maria <mjsanchezalmeyra@gmail.com>
2022-09-27 16:44:50 +0200

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__banner">
+        <h5>GET THE BOOK !</h5>
+      </div>
+      <div className="header__title">
+        <h1>MICRODOSIS</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
